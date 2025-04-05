93811d29a8c072b958d0a3c11d73c2b7bdc72f97
FaridRadit <162663946+FaridRadit@users.noreply.github.com>
2025-04-05 13:38:22 +0700

    Update DataNote.js

--- src/pages/DataNote.js
+++ src/pages/DataNote.js
@@ -14,7 +14,7 @@ function Noteuser() {
 
   const getNote = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/getNote");
+      const response = await axios.get("http://34.72.240.54/getNote");
       setNote(response.data);
     } catch (error) {
       console.error("Error fetching notes:", error);
@@ -23,7 +23,7 @@ function Noteuser() {
 
   const deleteNote = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/delete-Note/${id}`); 
+      await axios.delete(`http://34.72.240.54/delete-Note/${id}`); 
       getNote(); // 
     } catch (error) {
       console.error("Error deleting note:", error);
